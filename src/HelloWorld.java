02faeac3ae6d34050deb461649694330393096ab
YAHDILLAH <adiyahdillah434@gmail.com>
2023-05-02 17:23:37 +0700

    initiate project

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        boolean test = true;
+        String testString = "string";
+
+        System.out.println("Hello Word - " + test + " - " + testString);
+    }
+}
